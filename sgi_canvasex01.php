88d068d2e7543eab72920198f98d9fdb20d7c4e4
Sergey Gasanov <sgimancs@gmail.com>
2023-10-11 17:50:07 +0300

    update
    update

--- sgi_canvasex01.php
+++ sgi_canvasex01.php
@@ -11,17 +11,47 @@ require_once ('jpgraph/jpgraph_canvas.php');
 // Настройте базовый холст, с которым мы сможем работать
 $g = new CanvasGraph(400,300,'auto');
 $g->SetMargin(5,11,6,11);
-$g->SetShadow();
-$g->SetMarginColor("teal");
+//$g->SetShadow();
+//$g->SetMarginColor("teal");
 
 // Нам нужно обвести область графика и поля, прежде чем добавлять
 // текст, так как в противном случае мы бы перезаписали текст.
-$g->InitFrame();
+//$g->InitFrame();
 
+//----------------------------------
 // Рисуем текстовое поле посередине
+//----------------------------------
+// TTF Font families:
+// FF_COURIER
+// FF_VERDANA
+// FF_TIMES
+// FF_COMIC
+// FF_ARIAL
+// FF_GEORGIA
+// FF_TREBUCHE
+//----------------------------------
+// Hebrew fonts
+// FF_DAVID
+// FF_MIRIAM
+// FF_AHRON
+//----------------------------------
+// Dejavu-fonts http://sourceforge.net/projects/dejavu
+// FF_DV_SANSSERIF
+// FF_DV_SERIF
+// FF_DV_SANSSERIFMONO
+// FF_DV_SERIFCOND
+// FF_DV_SANSSERIFCOND
+
 $txt="This\nis\na TEXT!!!";
-$t = new Text($txt,200,10);
-$t->SetFont(FF_ARIAL,FS_BOLD,40);
+$t = new Text($txt, 200, 50);
+
+//$t->SetFont(FF_ARIAL,FS_BOLD,40);
+//$t->SetFont(FF_COMIC,FS_BOLD,40);
+//$t->SetFont(FF_VERDANA,FS_BOLD,40);
+//$t->SetFont(FF_TREBUCHE,FS_BOLD,40);
+
+$t->SetFont(FF_COMIC,FS_BOLD,40);
+
 
 // Как текстовое поле должно интерпретировать координаты?
 $t->Align('center','top');
@@ -30,7 +60,7 @@ $t->Align('center','top');
 $t->ParagraphAlign('center');
 
 // Добавляем рамку вокруг текста, белую заливку, черную рамку и серую тень
-$t->SetBox("white","black","gray");
+//$t->SetBox("white","black","gray");
 
 // Вывод текста
 $t->Stroke($g->img);
